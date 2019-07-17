8a093d4d61b934865e20fccac43d82cda6f9eb5a
TharunMV <37066939+TharunMV@users.noreply.github.com>
2019-07-17 10:52:09 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+  char ch;
+  int asc;
+  scanf("%c",&ch);
+  asc = (int)ch;
+  if(asc>=97 && asc<=122)
+  printf("Alphabet\n");
+  else
+  printf("Invalid\n");
+  return 0;
+} 
