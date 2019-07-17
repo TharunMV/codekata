c27544746e014a187d5c74b0688ca0d22bdbdccf
TharunMV <37066939+TharunMV@users.noreply.github.com>
2019-07-17 10:15:12 +0530

    Create num.c

---
+++ num.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+  int n;
+  scanf("%d",&n);
+  if(n>0)
+  printf("Positive\n");
+  if(n<0)
+  printf("Negative\n");
+  if(n==0)
+  printf("Zero\n");
+  return 0;
+}  
+  
