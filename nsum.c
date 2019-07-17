13af640ccfdac6ffc24cde3225e2873c74605ffe
TharunMV <37066939+TharunMV@users.noreply.github.com>
2019-07-17 11:57:41 +0530

    Update nsum.c

--- nsum.c
+++ nsum.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main() {
-  int n;
+  int n,sum=0;
   scanf("%d",&n);
-  for(int i=0;i<n;i++)
+  for(int i=1;i<=n;i++)
   sum = sum + i;
   printf("%d\n",sum);
   return 0;
-}  
+} 
