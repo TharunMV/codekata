e325dc02bfc5e44e78a7aa5b902908ae096a8cf9
TharunMV <37066939+TharunMV@users.noreply.github.com>
2019-07-17 14:47:42 +0530

    Update and rename num.py to num.c

---
+++ num.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    if(n==0)
+    printf("Zero\n");
+    if(n>0)
+    printf("Positive\n");
+    if(n<0)
+    printf("Negative\n");
+    return 0;
+}    
--- num.py
+++
@@ -1,8 +0,0 @@
-ch=int(input())
-if (ch>0):
-    print("Positive")
-elif(ch<0):
-    print("Negative")
-else:
-    print("ZERO")
-  
